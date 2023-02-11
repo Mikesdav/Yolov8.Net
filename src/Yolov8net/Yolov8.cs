efbe75cdcfc8424a593378b8844a79c5526f0962
Mikesdav <davidmikesel@gmail.com>
2023-02-11 11:16:58 -0600

    ParseOutput doesn't need parallelism

--- src/Yolov8net/Yolov8.cs
+++ src/Yolov8net/Yolov8.cs
@@ -19,51 +19,46 @@ namespace Yolov8Net
 
         protected List<Prediction> ParseOutput(DenseTensor<float> output, Image image)
         {
-            var result = new ConcurrentBag<Prediction>();
+            var result = new List<Prediction>((int)(output.Length / output.Dimensions[1]));
+            int labelsCount = ModelOutputDimensions - 4;
 
-            var (w, h) = (image.Width, image.Height); // image w and h
-            var (xGain, yGain) = (ModelInputWidth / (float)w, ModelInputHeight / (float)h); // x, y gains
-            var gain = Math.Min(xGain, yGain); // gain = resized / original
+            var (w, h) = (image.Width, image.Height);
+            var (xGain, yGain) = (ModelInputWidth / (float)w, ModelInputHeight / (float)h);
+            var gain = Math.Min(xGain, yGain);
+            var (xPad, yPad) = ((ModelInputWidth - w * gain) / 2, (ModelInputHeight - h * gain) / 2);
 
-            var (xPad, yPad) = ((ModelInputWidth - w * gain) / 2, (ModelInputHeight - h * gain) / 2); // left, right pads
-
-            //for each batch
-            Parallel.For(0, output.Dimensions[0], i =>
+            for (int i = 0; i < output.Dimensions[0]; i++)
             {
-                //divide total length by the elements per prediction
-                Parallel.For(0, (int)(output.Length / output.Dimensions[1]), j =>
+                for (int j = 0; j < output.Length / output.Dimensions[1]; j++)
                 {
+                    float xMin = ((output[i, 0, j] - output[i, 2, j] / 2) - xPad) / gain;
+                    float yMin = ((output[i, 1, j] - output[i, 3, j] / 2) - yPad) / gain;
+                    float xMax = ((output[i, 0, j] + output[i, 2, j] / 2) - xPad) / gain;
+                    float yMax = ((output[i, 1, j] + output[i, 3, j] / 2) - yPad) / gain;
 
-                    float xMin = ((output[i, 0, j] - output[i, 2, j] / 2) - xPad) / gain; // unpad bbox tlx to original
-                    float yMin = ((output[i, 1, j] - output[i, 3, j] / 2) - yPad) / gain; // unpad bbox tly to original
-                    float xMax = ((output[i, 0, j] + output[i, 2, j] / 2) - xPad) / gain; // unpad bbox brx to original
-                    float yMax = ((output[i, 1, j] + output[i, 3, j] / 2) - yPad) / gain; // unpad bbox bry to original
+                    xMin = Utils.Clamp(xMin, 0, w - 0);
+                    yMin = Utils.Clamp(yMin, 0, h - 0);
+                    xMax = Utils.Clamp(xMax, 0, w - 1);
+                    yMax = Utils.Clamp(yMax, 0, h - 1);
 
-                    xMin = Utils.Clamp(xMin, 0, w - 0); // clip bbox tlx to boundaries
-                    yMin = Utils.Clamp(yMin, 0, h - 0); // clip bbox tly to boundaries
-                    xMax = Utils.Clamp(xMax, 0, w - 1); // clip bbox brx to boundaries
-                    yMax = Utils.Clamp(yMax, 0, h - 1); // clip bbox bry to boundaries
-
-                    Parallel.For(0, ModelOutputDimensions - 4, l =>
+                    for (int l = 0; l < labelsCount; l++)
                     {
                         var pred = output[i, 4 + l, j];
-
-                        //skip low confidence values
-                        if (pred < Confidence) return;
-
-                        result.Add(new Prediction()
+                        if (pred >= Confidence)
                         {
-                            Label = Labels[l],
-                            Score = pred,
-                            Rectangle = new RectangleF(xMin, yMin, xMax - xMin, yMax - yMin)
-                        });
-                    });
-                });
-            });
+                            result.Add(new Prediction()
+                            {
+                                Label = Labels[l],
+                                Score = pred,
+                                Rectangle = new RectangleF(xMin, yMin, xMax - xMin, yMax - yMin)
+                            });
+                        }
+                    }
+                }
+            }
 
-            return result.ToList();
+            return result;
         }
-
         override public Prediction[] Predict(Image image)
         {
             return Suppress(
